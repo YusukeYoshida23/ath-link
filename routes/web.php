5fd76468003c020e1f44bf58e4358fabdb1e5b5b
YusukeYoshida23 <not_over_yet.1123@hotmail.co.jp>
2024-01-31 22:22:34 +0900

    Chapter4終了

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test',[TestController::class, 'test'])
+->name('test');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
