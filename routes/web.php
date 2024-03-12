b1bd9bce99a59bb672880c3e9730db3f6fbab885
YusukeYoshida23 <not_over_yet.1123@hotmail.co.jp>
2024-03-12 09:45:35 +0900

    上級編、アカウント編集画面終わり

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ContactController;
 
 
+Route::get('post/mypost', [PostController::class, 'mypost'])->name('post.mypost');
+Route::get('post/mycomment', [PostController::class, 'mycomment'])->name('post.mycomment');
+Route::resource('post', PostController::class);
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,30 +27,28 @@ Route::get('/', function () {
     return view('welcome');
 })->name('top');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::get('/dashboard', function () {
-    //     return view('dashboard');
-    // })->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth', 'can:admin')->group(function () {
+    Route::get('/profile/index', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::post('post/comment/store', [CommentController::class, 'store'])->name('comment.store');
+
+
+Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 
     // お問い合わせ
 Route::controller(ContactController::class)->group(function(){
     Route::get('contact/create', 'create')->name('contact.create')->middleware('guest');
     Route::post('contact/store', 'store')->name('contact.store');
-});
-
-    // ログイン後の通常のユーザー画面
-Route::middleware(['verified'])->group(function(){
-    Route::get('post/mypost', [PostController::class, 'mypost'])->name('post.mypost');
-    Route::get('post/mycomment', [PostController::class, 'mycomment'])->name('post.mycomment');
-    Route::resource('post', PostController::class);
-    Route::post('post/comment/store', [CommentController::class, 'store'])->name('comment.store');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    // 管理者用画面
-    Route::middleware('auth', 'can:admin')->group(function () {
-        Route::get('/profile/index', [ProfileController::class, 'index'])->name('profile.index');
-    });
 });
 
 require __DIR__.'/auth.php';
